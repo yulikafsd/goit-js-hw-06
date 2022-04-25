d3d3ca1ea52fd22fe354d706d1b4fa1e2a7b29b8
Yulika Zagorovsky <ju.zagorovsky@gmail.com>
2022-04-26 01:57:42 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,20 +1,48 @@
-// Напиши скрипт створення і очищення колекції елементів.
-// Користувач вводить кількість елементів в input і натискає кнопку Створити,після чого рендериться колекція.
-// Натисненням на кнопку Очистити, колекція елементів очищається.
-// Створи функцію createBoxes(amount), яка приймає один параметр - число. 
-// Функція створює стільки < div >, скільки вказано в amount і додає їх у div#boxes.
 // Розміри найпершого <div> - 30px на 30px.
 // Кожен елемент після першого повинен бути ширшим і вищим від попереднього на 10px.
 // Всі елементи повинні мати випадковий колір фону у форматі HEX. 
-// Використовуй готову функцію getRandomHexColor для отримання кольору.
 
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215)
-//     .toString(16)
-//     .padStart(6, 0)}`;
-// }
-// Створи функцію destroyBoxes(), яка очищає вміст div#boxes, у такий спосіб видаляючи всі створені елементи.
+const refs = {
+  inputEl: document.querySelector('[type="number"]'),
+  createBtnEl: document.querySelector('[data-create]'),
+  destroyBtnEl: document.querySelector('[data-destroy]'),
+  boxesEl: document.querySelector('#boxes'),
+};
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+const getRandomHexColor = function () {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+refs.createBtnEl.addEventListener('click', function createBoxes (amount) {
+  amount = refs.inputEl.value;
+
+  // Для удобства проверки результата:
+  const arrNumbers = [];
+  for (let i = 0; i < amount; i += 1) {
+    const divBox = `<div>DivBox${i + 1}</div>`;
+    arrNumbers.push(divBox);
+  };
+
+  const boxesMarkup = arrNumbers
+    .map((arrNumber) => arrNumber)
+    .join("");
+  
+  refs.boxesEl.innerHTML = boxesMarkup;
+
+  // Перебрать дивы-дети boxes и задать им размер и цвет... Как???
+  // let j = 30;
+  // const box = document.querySelector('[class="box${i+1}"]') - как перебирать?
+  // box.style.backgroundColor = getRandomHexColor;
+  // box.style.width = j + "px";
+  // box.style.height = j + "px";
+  // j += 10;
+  
+});
+
+refs.destroyBtnEl.addEventListener('click', function destroyBoxes () {
+  refs.boxesEl.innerHTML = "";
+  refs.inputEl.value = "";
+  console.log(refs.boxesEl);
+});
