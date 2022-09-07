bfd55e22940c0ae4205fc5d8b5eccd88f9cca554
Yulika Zagorovsky <ju.zagorovsky@gmail.com>
2022-09-07 14:34:31 +0200

    Update task-06.js
    Adds alternative clearer code

--- js/task-06.js
+++ js/task-06.js
@@ -2,24 +2,52 @@ const inputEl = document.querySelector('#validation-input');
 const dataLength = Number(inputEl.dataset.length);
 
 inputEl.addEventListener('blur', onBlur);
-inputEl.addEventListener('focus', onFocus);
 
 function onBlur(event) {
   const eventClass = event.currentTarget.classList;
 
-  event.currentTarget.value.length < dataLength
-    ? eventClass.add('invalid')
-    : eventClass.add('valid');
-}
+  if (event.currentTarget.value.length < dataLength) {
+    if (eventClass.contains('valid')) {
+      eventClass.replace('valid', 'invalid');
+    }
 
-function onFocus(event) {
-  const eventClass = event.currentTarget.classList;
-
-  if (eventClass.contains('valid')) {
-    eventClass.remove('valid');
+    if (!eventClass.contains('invalid')) {
+      eventClass.add('invalid');
+    }
   }
 
-  if (eventClass.contains('invalid')) {
-    eventClass.remove('invalid');
+  if (event.currentTarget.value.length >= dataLength) {
+    if (eventClass.contains('invalid')) {
+      eventClass.replace('invalid', 'valid');
+    }
+
+    if (!eventClass.contains('valid')) {
+      eventClass.add('valid');
+    }
   }
 }
+
+// Другий варіант - скидати класи на фокусі
+// (закоментити стр. 6 - 32, розкоментити 37-57)
+
+// inputEl.addEventListener('focus', onFocus);
+
+// function onBlur(event) {
+//   const eventClass = event.currentTarget.classList;
+
+//   event.currentTarget.value.length < dataLength
+//     ? eventClass.add('invalid')
+//     : eventClass.add('valid');
+// }
+
+// function onFocus(event) {
+//   const eventClass = event.currentTarget.classList;
+
+//   if (eventClass.contains('valid')) {
+//     eventClass.remove('valid');
+//   }
+
+//   if (eventClass.contains('invalid')) {
+//     eventClass.remove('invalid');
+//   }
+// }
