d99c72e5aaab2cec640e334400d1a2d516575439
yulikafsd <ju.zagorovsky@gmail.com>
2026-04-23 23:41:05 +0200

    add tasks solutions

--- js/task-2.js
+++ js/task-2.js
@@ -1,1 +1,31 @@
 'use strict';
+
+class Storage {
+    #items;
+
+    constructor(items) {
+        this.#items = items;
+    }
+
+    getItems() {
+        return this.#items;
+    }
+
+    addItem(newItem) {
+        this.#items.push(newItem);
+    }
+
+    removeItem(itemToRemove) {
+        const idx = this.#items.indexOf(itemToRemove);
+        if (idx >= 0) {
+            this.#items.splice(idx, 1);
+        }
+    }
+}
+
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem('Droid');
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem('Prolonger');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
